c76cedbbfd2cd1252ac802a7d99913aa4f8001f2
Vitalijus Desukas <vitalij.desuk@gmail.com>
2025-10-27 13:25:54 +0200

    integrate stripe webhooks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get "pages/home"
   resources :companies
+
+  # Stripe webhooks endpoint
+  post "/stripe_webhook", to: "webhooks#stripe_webhook"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
