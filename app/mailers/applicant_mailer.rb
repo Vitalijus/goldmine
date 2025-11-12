97469e1848c4432d0900ef2ea78c0963de4bcea0
Vitalijus Desukas <vitalij.desuk@gmail.com>
2025-11-12 11:30:12 +0200

    improve applicant mailer conversion

--- app/mailers/applicant_mailer.rb
+++ app/mailers/applicant_mailer.rb
@@ -1,8 +1,9 @@
 class ApplicantMailer < ApplicationMailer
-    default from: "rorlist.com <support@rorlist.com>"
+  helper :pages
+  default from: "rorlist.com <support@rorlist.com>"
 
-    def new_applicant_email(applicant)
-      @applicant = applicant
-      mail(to: @applicant.email, subject: "Thank you for applying!")
-    end
+  def new_applicant_email(applicant)
+    @applicant = applicant
+    mail(to: @applicant.email, subject: "Thank you for applying!")
+  end
 end
