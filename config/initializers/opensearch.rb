71ca04056c3e5909585b65b7a549d175b2d51433
Vitalijus Desukas <vitalij.desuk@gmail.com>
2025-12-10 13:23:47 +0200

    update opensearch initializer

--- config/initializers/opensearch.rb
+++ config/initializers/opensearch.rb
@@ -1,11 +1,12 @@
 require 'opensearch-ruby'
 
-OPENSEARCH_CLIENT = OpenSearch::Client.new(
-  host: ENV['OPENSEARCH_URL'] || 'https://localhost:9200',
-  user: ENV['OPENSEARCH_USER'] || 'admin',
-  password: ENV['OPENSEARCH_PASSWORD'] || 'admin',
-  log: true,
-  transport_options: {
-    ssl: { verify: false }# For testing only. Use certificate for validation.
-  }
-)
+if Rails.env.production?
+  OPENSEARCH_CLIENT = OpenSearch::Client.new(url: ENV['BONSAI_URL'])
+else
+  OPENSEARCH_CLIENT = OpenSearch::Client.new(
+    host: 'https://localhost:9200',
+    user: 'admin',
+    password: 'admin',
+    transport_options: { ssl: { verify: false } }
+  )
+end
