d420d30d58cfa2c6201f53ec22e6db674a7f359b
Vitalijus Desukas <vitalij.desuk@gmail.com>
2026-01-30 12:47:40 +0200

    add partials and update controller Careers

--- config/initializers/opensearch.rb
+++ config/initializers/opensearch.rb
@@ -1,10 +1,13 @@
+# To start OpenSearch locally run: opensearch
+# Keep in mind this will establish insecure connection
+
 require 'opensearch-ruby'
 
 if Rails.env.production?
   OPENSEARCH_CLIENT = OpenSearch::Client.new(url: ENV['BONSAI_URL'])
 else
   OPENSEARCH_CLIENT = OpenSearch::Client.new(
-    host: 'https://localhost:9200',
+    host: 'http://localhost:9200',
     user: 'admin',
     password: 'admin',
     transport_options: { ssl: { verify: false } }
