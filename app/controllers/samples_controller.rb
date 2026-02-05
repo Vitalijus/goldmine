2f2c1aef129cd0cd5a39e3671efb08c1a4079ace
Vitalijus Desukas <vitalij.desuk@gmail.com>
2026-02-05 21:05:29 +0200

    update sample controller

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -1,26 +1,44 @@
 class SamplesController < ApplicationController
+  before_action :load_countries, only: %i[new create]
+    
+  # GET /samples/new
   def new
     @sample = Sample.new
-    get_countries = Opensearch::GetCountriesQuery.new(countries: params[:countries],
-                                                      frameworks: params[:frameworks],
-                                                      languages: params[:programming_languages],
-                                                      other_tech: params[:other_tech_stack],
-                                                      remote: params[:remote],
-                                                      size: 10)
-    @countries = get_countries.build_result
   end
 
+  # POST /samples or /samples.json
   def create
     @sample = Sample.new(sample_params)
 
     respond_to do |format|
       if @sample.save
         format.html { redirect_to new_sample_path, notice: "Sample request successfully created." }
-        format.json { render :show, status: :created, location: @company }
+        format.json { render :new, status: :created, location: @sample }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @sample.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  private
+
+  # Load countries for form selection
+  def load_countries
+    get_countries = Opensearch::GetCountriesQuery.new(
+      countries: params[:countries],
+      frameworks: params[:frameworks],
+      languages: params[:programming_languages],
+      other_tech: params[:other_tech_stack],
+      remote: params[:remote],
+      size: 10
+    )
+
+    @countries = get_countries.build_result
+  end
+
+  # Only allow a list of trusted parameters through.
+  def sample_params
+    params.expect(sample: [ :email, :programming_languages, :frameworks, :other_tech_stack, :countries ])
   end
 end
--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,2 +1,3 @@
 class Sample < ApplicationRecord
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "is not a valid email address"}
 end
