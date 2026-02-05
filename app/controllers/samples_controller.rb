2be9770e1e5d07dbd157989603b57fb86047e9b4
Vitalijus Desukas <vitalij.desuk@gmail.com>
2026-02-05 20:14:20 +0200

    move filters to shared folder

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -1,8 +1,26 @@
 class SamplesController < ApplicationController
   def new
     @sample = Sample.new
+    get_countries = Opensearch::GetCountriesQuery.new(countries: params[:countries],
+                                                      frameworks: params[:frameworks],
+                                                      languages: params[:programming_languages],
+                                                      other_tech: params[:other_tech_stack],
+                                                      remote: params[:remote],
+                                                      size: 10)
+    @countries = get_countries.build_result
   end
 
   def create
+    @sample = Sample.new(sample_params)
+
+    respond_to do |format|
+      if @sample.save
+        format.html { redirect_to new_sample_path, notice: "Sample request successfully created." }
+        format.json { render :show, status: :created, location: @company }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @sample.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
