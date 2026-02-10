8d0fb3689628e0a1b08855dd0e8adcfcd87df5bc
Vitalijus Desukas <vitalij.desuk@gmail.com>
2026-02-10 13:46:04 +0200

    add schedulker worker and update procfile

--- app/sidekiq/sample_job.rb
+++ app/sidekiq/sample_job.rb
@@ -5,7 +5,12 @@ class SampleJob
   def perform(sample_id)
     sample = Sample.find_by(id: sample_id)
     return unless sample
-  
+    # Guard to prevent duplicate emails
+    return if sample.updated_at && sample.updated_at > 1.month.ago
+
     SampleMailer.updated_sample_email(sample).deliver_now
+    
+    # Update the sample's updated_at timestamp to prevent sending multiple emails for the same sample
+    sample.update(updated_at: Time.current)
   end
 end
